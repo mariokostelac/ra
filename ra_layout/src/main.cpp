25bcd1a8ce5d9180b17fa40439656c19c6628f21
rvaser <robert.vaser@fer.hr>
2015-06-09 16:46:01 +0200

    [ra_layout] Implemented functionality

--- ra_layout/src/main.cpp
+++ ra_layout/src/main.cpp
@@ -3,9 +3,91 @@
 
 #include "ra/ra.hpp"
 
+const struct option options[] = {
+    {"reads", required_argument, 0, 'i'},
+    {"overlaps", required_argument, 0, 'j'},
+    {"contigs-out", required_argument, 0, 'c'},
+    {"help", no_argument, 0, 'h'},
+    {0, 0, 0, 0}
+};
+
+static void help();
+
 int main(int argc, char* argv[]) {
 
-    printf("Layout phase\n");
+    char* readsPath = nullptr;
+    char* overlapsPath = nullptr;
+
+    char* contigsOut = nullptr;
+
+    while (1) {
+
+        char argument = getopt_long(argc, argv, "i:j:h", options, nullptr);
+
+        if (argument == -1) {
+            break;
+        }
+
+        switch (argument) {
+        case 'i':
+            readsPath = optarg;
+            break;
+        case 'j':
+            overlapsPath = optarg;
+            break;
+        case 'c':
+            contigsOut = optarg;
+            break;
+        default:
+            help();
+            return -1;
+        }
+    }
+
+    ASSERT(readsPath, "IO", "missing option -i (reads file)");
+    ASSERT(overlapsPath, "IO", "missing option -j (overaps file)");
+
+    std::vector<Read*> reads;
+    readAfgReads(reads, readsPath);
+
+    std::vector<Overlap*> overlaps;
+    readAfgOverlaps(overlaps, overlapsPath);
+
+    StringGraph* graph = new StringGraph(reads, overlaps);
+
+    graph->simplify();
+
+    std::vector<Overlap*> finalized;
+    graph->extractOverlaps(finalized);
+
+    writeAfgOverlaps(finalized, contigsOut == nullptr ? "layout.afg" : contigsOut);
+
+    delete graph;
+
+    for (const auto& it : overlaps) delete it;
+
+    for (const auto& it : reads) delete it;
 
     return 0;
+}
+
+static void help() {
+
+    printf(
+    "usage: ra_layout -i <reads file> -j <overlaps file> [arguments ...]\n"
+    "\n"
+    "arguments:\n"
+    "    -i, --reads <file>\n"
+    "        (required)\n"
+    "        input afg reads file\n"
+    "    -j, --overlaps <file>\n"
+    "        (required)\n"
+    "        input afg overlaps file\n"
+    "    -t, --threads <int>\n"
+    "        default: approx. number of processors/cores\n"
+    "        number of threads used\n"
+    "    --contigsOut <file>\n"
+    "        output afg contigs file\n"
+    "    -h, -help\n"
+    "        prints out the help\n");
 }
