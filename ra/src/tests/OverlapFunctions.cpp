ecde1a89ad533648e4e4e6f50b62eadd48066dbd
Mario Kostelac <mario.kostelac@gmail.com>
2015-09-26 14:56:40 +0100

    add tests for calc_forced_hangs

--- ra/src/tests/OverlapFunctions.cpp
+++ ra/src/tests/OverlapFunctions.cpp
@@ -40,3 +40,97 @@ TEST(FilterTransitives, SimpleTest1) {
   ASSERT_EQ(dst.end(), std::find(dst.begin(), dst.end(), one_three));
   ASSERT_EQ(2, dst.size());
 }
+
+//std::pair<int, int> calc_forced_hangs(uint32_t a_lo, uint32_t a_hi, uint32_t a_len, bool a_rc,
+    //uint32_t b_lo, uint32_t b_hi, uint32_t b_len, bool b_rc);
+
+TEST(CalcForcedHangs, SimpleTest1) {
+  // -|-|>
+  //  |-|->
+  int a_len = 3, b_len = 3;
+  int a_lo = 1, a_hi = 2;
+  int b_lo = 0, b_hi = 1;
+  bool a_rc = false, b_rc = false;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(1, hangs.first);
+  ASSERT_EQ(1, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest2) {
+  // -|---|>
+  //  |---|->
+  int a_len = 5, b_len = 5;
+  int a_lo = 1, a_hi = 4;
+  int b_lo = 0, b_hi = 3;
+  bool a_rc = false, b_rc = false;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(1, hangs.first);
+  ASSERT_EQ(1, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest3) {
+  // --|--|>
+  //  -|--|-->
+  int a_len = 5, b_len = 6;
+  int a_lo = 2, a_hi = 4;
+  int b_lo = 1, b_hi = 3;
+  bool a_rc = false, b_rc = false;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(1, hangs.first);
+  ASSERT_EQ(2, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest4) {
+  // --|--|--->
+  //  -|--|-->
+  int a_len = 8, b_len = 6;
+  int a_lo = 2, a_hi = 4;
+  int b_lo = 1, b_hi = 3;
+  bool a_rc = false, b_rc = false;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(1, hangs.first);
+  ASSERT_EQ(-1, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest5) {
+  //  -|--|-->
+  // --|--|--->
+  int a_len = 6, b_len = 8;
+  int a_lo = 1, a_hi = 3;
+  int b_lo = 2, b_hi = 4;
+  bool a_rc = false, b_rc = false;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(-1, hangs.first);
+  ASSERT_EQ(1, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest6) {
+  // --|--|>
+  //  <|--|---
+  int a_len = 5, b_len = 6;
+  int a_lo = 2, a_hi = 4;
+  int b_lo = 3, b_hi = 5;
+  bool a_rc = false, b_rc = true;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(1, hangs.first);
+  ASSERT_EQ(2, hangs.second);
+}
+
+TEST(CalcForcedHangs, SimpleTest7) {
+  //  |--|>
+  // <|--|---
+  int a_len = 3, b_len = 6;
+  int a_lo = 0, a_hi = 2;
+  int b_lo = 3, b_hi = 5;
+  bool a_rc = false, b_rc = true;
+  auto hangs = calc_forced_hangs(a_lo, a_hi, a_len, a_rc, b_lo, b_hi, b_len, b_rc);
+
+  ASSERT_EQ(-1, hangs.first);
+  ASSERT_EQ(2, hangs.second);
+}
