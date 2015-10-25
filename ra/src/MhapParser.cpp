6ca3109992f2493105289ecb4cdf8cb96b33e584
rvaser <robert.vaser@fer.hr>
2015-10-25 15:45:44 +0100

    [MhapParser] Variable name change which caused the bug

--- ra/src/MhapParser.cpp
+++ ra/src/MhapParser.cpp
@@ -10,23 +10,23 @@ namespace MHAP {
     uint32_t b_id;
     double jaccard_score;
     double shared_minmers;
-    uint32_t a_fwd;
+    uint32_t a_rc;
     uint32_t a_lo;
     uint32_t a_hi;
     uint32_t a_len;
-    uint32_t b_fwd;
+    uint32_t b_rc;
     uint32_t b_lo;
     uint32_t b_hi;
     uint32_t b_len;
 
     while (true) {
       input >> a_id >> b_id >> jaccard_score >> shared_minmers;
-      input >> a_fwd >> a_lo >> a_hi >> a_len;
-      input >> b_fwd >> b_lo >> b_hi >> b_len;
+      input >> a_rc >> a_lo >> a_hi >> a_len;
+      input >> b_rc >> b_lo >> b_hi >> b_len;
 
       // update params to fit Overlap
       a_hi += 1;
-      if (b_fwd) {
+      if (b_rc) {
           auto tmp = b_lo;
           b_lo = b_len - (b_hi + 1);
           b_hi = b_len - tmp;
@@ -35,8 +35,8 @@ namespace MHAP {
       }
 
       auto overlap = new Overlap(
-          reads[a_id], a_lo, a_hi, a_fwd, // [a_lo, a_hi>
-          reads[b_id], b_lo, b_hi, b_fwd  // [b_lo, b_hi>
+          reads[a_id], a_lo, a_hi, a_rc,
+          reads[b_id], b_lo, b_hi, b_rc
       );
 
       if (input.fail()) {
