ef935408464b243dfba7bdfea25c747c202f928c
Mario Kostelac <mario@intercom.io>
2015-11-27 18:10:26 +0000

    Add some statistics printing before and after filtering based on absolute error rate.

--- filter_erroneous_overlaps/src/main.cpp
+++ filter_erroneous_overlaps/src/main.cpp
@@ -54,7 +54,7 @@ void write_specs_to(const string path) {
 void filter_overlaps_by_absolute_errate(OverlapSet* overlaps, double upper_limit) {
   fprintf(stderr, "Filtering overlaps with errate > %lf\n", upper_limit);
 
-  int next_position = 0;
+  int next_position = 0, size_before = overlaps->size();
   for (int i = 0; i < (int) overlaps->size(); ++i) {
     auto o = (*overlaps)[i];
     if (o->err_rate() >= upper_limit) continue;
@@ -64,6 +64,38 @@ void filter_overlaps_by_absolute_errate(OverlapSet* overlaps, double upper_limit
   }
 
   overlaps->resize(next_position);
+
+  int diff = size_before - overlaps->size();
+  fprintf(stderr, "Filtered %d overlaps (%lf %%)\n", diff, 1.0 * diff / size_before);
+}
+
+void print_stats(OverlapSet& overlaps) {
+  fprintf(stderr, "Statistic\terror_rate\tcount\n");
+
+  int index = 0.5 * overlaps.size();
+  fprintf(stderr, "p50\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.6 * overlaps.size();
+  fprintf(stderr, "p60\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.7 * overlaps.size();
+  fprintf(stderr, "p70\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.8 * overlaps.size();
+  fprintf(stderr, "p80\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.9 * overlaps.size();
+  fprintf(stderr, "p90\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.95 * overlaps.size();
+  fprintf(stderr, "p95\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+
+  index = 0.99 * overlaps.size();
+  fprintf(stderr, "p99\t%lf\t%d\n", overlaps[index]->err_rate(), index);
+}
+
+bool compare_by_errate(Overlap* a, Overlap* b) {
+  return a->err_rate() < b->err_rate();
 }
 
 int main(int argc, char **argv) {
@@ -83,7 +115,11 @@ int main(int argc, char **argv) {
   depot.load_overlaps(overlaps, reads);
   fprintf(stderr, "Read %lu overlaps\n", overlaps.size());
 
+  sort(overlaps.begin(), overlaps.end(), compare_by_errate);
+
+  print_stats(overlaps);
   filter_overlaps_by_absolute_errate(&overlaps, MAX_ABSOLUTE_ERRATE);
+  print_stats(overlaps);
 
   fprintf(stderr, "Updating depot...\n");
   depot.store_overlaps(overlaps);
